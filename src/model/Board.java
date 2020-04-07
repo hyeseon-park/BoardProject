9ef29982b5984e71e12604f0cb9358dcefb8f3d8
hyeseon-park <lightning350@naver.com>
2020-04-07 16:05:13 +0900

    Board model 완성

---
+++ src/model/Board.java
@@ -0,0 +1,44 @@
+package model;
+
+public class Board {
+	private int num;
+	private String title;
+	private String content;
+	private String pass;
+	private String name;
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Board [num=" + num + ", title=" + title + ", content=" + content + ", pass=" + pass + ", name=" + name + "]";
+	}
+}
