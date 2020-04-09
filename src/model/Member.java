3c93719e6f0e8503182d55f6e345b0157e88ef7f
hyeseon-park <lightning350@naver.com>
2020-04-09 14:34:41 +0900

    Member model 완성

---
+++ src/model/Member.java
@@ -0,0 +1,37 @@
+package model;
+
+public class Member {
+	private int mNum;
+	private String mID;
+	private String mPass;
+	private String mName;
+	
+	public int getmNum() {
+		return mNum;
+	}
+	public void setmNum(int mNum) {
+		this.mNum = mNum;
+	}
+	public String getmID() {
+		return mID;
+	}
+	public void setmID(String mID) {
+		this.mID = mID;
+	}
+	public String getmPass() {
+		return mPass;
+	}
+	public void setmPass(String mPass) {
+		this.mPass = mPass;
+	}
+	public String getmName() {
+		return mName;
+	}
+	public void setmName(String mName) {
+		this.mName = mName;
+	}
+	@Override
+	public String toString() {
+		return "Member [mNum=" + mNum + ", mID=" + mID + ", mPass=" + mPass + ", mName=" + mName + "]";
+	}
+}
