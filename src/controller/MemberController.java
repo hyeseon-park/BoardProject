0cd7524c348c42964203fceb4eac325fe7307b66
hyeseon-park <lightning350@naver.com>
2020-04-09 14:38:37 +0900

    MemberController 생성

---
+++ src/controller/MemberController.java
@@ -0,0 +1,49 @@
+package controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import model.Member;
+import service.MemberService;
+
+@Controller
+public class MemberController {
+	@Autowired
+	MemberService memberService;
+
+	@RequestMapping(value = "/signUp", method = RequestMethod.POST)
+	public String signUp(Member member, Model model) {
+		boolean result = memberService.signUpMember(member);
+		String msg = "";
+		if (result) {
+			msg = "가입 성공했습니다.";
+		} else {
+			msg = "가입 실패했습니다.";
+		}
+		model.addAttribute("msg", msg);
+		return "member/signInForm";
+	}
+
+	@RequestMapping(value = "/signUp", method = RequestMethod.GET)
+	public String showSignUp() {
+		return "member/signUpForm";
+	}
+
+	@RequestMapping(value = "/signIn", method = RequestMethod.POST)
+	public String signIn(Model model, String mID, String mPass) {
+		if (mPass.equals(memberService.getMemberByID(mID).getmPass())) {
+			return "redirect:list";
+		} else {
+			model.addAttribute("msg", "로그인 실패했습니다.");
+			return "member/signInForm";
+		}
+	}
+
+	@RequestMapping(value = "/signIn", method = RequestMethod.GET)
+	public String showSignIn() {
+		return "member/signInForm";
+	}
+}
