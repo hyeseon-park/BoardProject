5b6998cd4f02edc74cf65a55cbb122aeba9a5982
hyeseon-park <lightning350@naver.com>
2020-04-19 15:38:22 +0900

    ReplyController 기본 틀 완성

---
+++ src/controller/ReplyController.java
@@ -0,0 +1,49 @@
+package controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import model.Reply;
+import service.ReplyService;
+
+@Controller
+@RequestMapping("/reply")
+public class ReplyController {
+	@Autowired
+	ReplyService replyService;
+	
+	@ResponseBody
+	@RequestMapping(value="/all/{boardNum}", method=RequestMethod.GET)
+	public List<Reply> ReplyList(Model model, @PathVariable("boardNum")int boardNum) {
+		List<Reply> replyList = replyService.getRepliesByBNum(boardNum);
+		return replyList;
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="/write", method=RequestMethod.POST)
+	public boolean write(Reply reply) {
+		boolean result = replyService.writeReply(reply);
+		return result;
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="/modify", method=RequestMethod.POST)
+	public boolean modify(Reply reply) {
+		boolean result = replyService.modifyReply(reply);
+		return result;
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="/delete", method=RequestMethod.POST)
+	public boolean delete(Reply reply) {
+		boolean result = replyService.removeReply(reply);
+		return result;
+	}
+}
